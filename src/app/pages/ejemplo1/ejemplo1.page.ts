6f9b7b5cfea7acdbd4c6458df6e41ca49d45bc0b
bilkis1212 <nicco.moreira03@gmail.com>
2021-09-03 13:03:26 -0400

    confirmar

--- src/app/pages/ejemplo1/ejemplo1.page.ts
+++ src/app/pages/ejemplo1/ejemplo1.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ejemplo1',
@@ -9,7 +9,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Ejemplo1Page implements OnInit {
 
-  constructor(private alertController: AlertController) { }
+  constructor(private router:Router, private alertController: AlertController) { }
 
   ngOnInit() {
     this.listar();
@@ -75,15 +75,53 @@ export class Ejemplo1Page implements OnInit {
   //metodos
   eliminar(rut:String){
     alert('selecciono eliminar' + rut);
+    var datos= localStorage.getItem('misdatos');
+    datos = datos.replace('[','');
+    datos = datos.replace(']','');
+    datos = datos.split('},{').join('};{');
+   // alert(datos);
+    var lista_temporal= new Array();
+    var per;
+    var arreglo_temp= datos.split(";");
+    for (let index = 0; index < arreglo_temp.length; index++) {    
+      var registro = arreglo_temp[index];
+      var la_persona = JSON.parse(registro);
+      per={
+        rut: la_persona.rut,
+        nombre: la_persona.nombre,
+        edad: la_persona.edad
+      };
+      if (la_persona.rut != rut) {
+        lista_temporal.push(per);
+      }
+      
+    }
+    this.lista_persona=lista_temporal;
+    localStorage.setItem('misdatos',JSON.stringify(lista_temporal));
   }
   actualizar(rut:String){
     alert('selecciono actualizar' + rut);
+    this.router.navigate(['/ejemplo2',rut])
   }
   listar(){
     var datos= localStorage.getItem('misdatos');
     datos = datos.replace('[','');
     datos = datos.replace(']','');
     datos = datos.split('},{').join('};{');
-    alert(datos);
+    //alert(datos);
+    var lista_temporal= new Array();
+    var per;
+    var arreglo_temp= datos.split(";");
+    for (let index = 0; index < arreglo_temp.length; index++) {    
+      var registro = arreglo_temp[index];
+      var la_persona = JSON.parse(registro);
+      per={
+        rut: la_persona.rut,
+        nombre: la_persona.nombre
+      };
+      lista_temporal.push(per);
+      
+    }
+    this.lista_persona=lista_temporal;
   }
 }
